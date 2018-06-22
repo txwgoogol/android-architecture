27720cfdb1659fbd3cd6f12d783d26622daeae41
txwgoogol <txwgoogol@gmail.com>
2018-06-22 14:40:55 +0800

    repair RxScheduler.java is null

---
+++ todo-mvp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/RxScheduler.java
@@ -0,0 +1,19 @@
+package com.example.android.architecture.blueprints.todoapp.util;
+
+import io.reactivex.ObservableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public final class RxScheduler {
+
+    /**
+     * 在RxJava的使用过程中我们会频繁的调用subscribeOn()和observeOn(),通过Transformer结合
+     * Observable.compose()我们可以复用这些代码
+     *
+     * @return Transformer
+     */
+    public static <T> ObservableTransformer<T, T> normalSchedulersTransformer() {
+        return observable -> observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+}
