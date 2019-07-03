47d9b47615176a78755cadd4f0f2f440c0883503
txwgoogol <txwgoogol@gmail.com>
2019-07-03 10:29:24 +0800

    add supress

--- todomvvm/app/src/main/java/com/example/todomvvm/data/source/local/ViewModelFactory.kt
+++ todomvvm/app/src/main/java/com/example/todomvvm/data/source/local/ViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class ViewModelFactory(private val weatherDao: WeatherDao) : ViewModelProvider.Factory {
 
+    @Suppress(names = ["UNCHECKED_CAST"])
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
             return WeatherViewModel(weatherDao) as T
