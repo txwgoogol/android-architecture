13923d21366d5362b7d3675302c159b25364fd2c
txwgoogol <txwgoogol@gmail.com>
2019-07-05 13:00:21 +0800

    add launcher & package

---
+++ todomvvm/app/src/main/java/com/example/todomvvm/ui/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.todomvvm.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todomvvm.R
+
+class SplashActivity : AppCompatActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_splash)
+
+//		val intent = Intent(this, MainActivity::class.java)
+//		startActivity(intent)
+//		this.finish()
+		
+	}
+	
+}
